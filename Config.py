274f46c9a13c1036b87e9ab8740b95d99b09436c
itzarjuna1 <pachangedinanath@gmail.com>
2025-06-09 19:30:10 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,17 +7,17 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID", "21195558"))
-API_HASH = getenv("56f56a1dd7a3c3c3c0ac1df8788ae32f")
+API_ID = int(getenv("API_ID", "22565342"))
+API_HASH = getenv("75e035926f72f2f4155a6f5f6e64be03")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("7732173919:AAGOQsF_f9rTMWL_gstdpF3qwSQAEra-Dtg")
+BOT_TOKEN = getenv("7201420162:AAGxAlHuNHtf_OZKKBwrAZhhD6vX6PB4HgM")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","ll_KU")
+OWNER_USERNAME = getenv("OWNER_USERNAME","ll_KUZE_ll")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME","Anya_lip_bot")
+BOT_USERNAME = getenv("BOT_USERNAME","Dark_x_musiczz_bot")
 # --------------------------------------------------------
-BOT_NAME = getenv("AnyaXMusic")
+BOT_NAME = getenv("˹ Dᴀʀᴋ ꭙ ᴋɴɪɢʜᴛ ˼ ♪ [ 𝟮𝟰/𝟳 ] ⚡˼ ♪")
 # ---------------------------------------------------------
 
 
@@ -27,10 +27,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://knight4563:knight4563@cluste
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002556228441))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002777786186))
 
 # Get this value from @PURVI_HELP_BOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 7495324168))
+OWNER_ID = int(getenv("OWNER_ID", 7926944005))
 
 
 ## Fill these variables if you're deploying on heroku.
@@ -41,15 +41,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Gxinfinity/ANYA_MUSIC",
+    "https://github.com/itzarjuna1/DARK",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/gxinfinity_support")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/infinitygx_bot_support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dark_x_knight_musiczz_support")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/dark_knight_support")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -71,7 +71,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
 
 # Get your pyrogram v2 session from @StringFatherBot on Telegram
-STRING1 = getenv("STRING_SESSION","BQFKJysAxVxAxgqf61WltaV3rS6KaeO03Mn0Ygv4U_OZJHQFXNd3CcbVfRBMYDyTVCIwQ2g7oyH7uFsQ8XwT_l0PSzowMKBHVTDXoERLJl6h-mueia1XFNeOPdtjMGtEc826AkID4FAcSVDqvcpFw7DZKgEpzPM51tV3o7XQzVH-FbaAfT1ILJMHMuFQ3xY_4qERyiAmuTkxS4c5K_l99Q0-drf5O6gnv4BhQmB7maftnqhruBFpqzRZkE2xUQ1kHJUAb9u1gVcekmOx-dgtCvccKBw54nEAhCuFjaYbDQS_MIGBgTF3_1HmNsQMqcv8JXSsNzkUjCGKY3rIxTEexI1zftYVPAAAAAHVyIxhAA")
+STRING1 = getenv("STRING_SESSION","BQFYUd4AaBQ-7rxbKMguDsyH8oJPUtsDN4JBK4J-P09mTvQkHm24IsRuD3VtdQi1nbxvR97mcLHog1Ke9s6dKIYskILAyeaNzfr7CayVeDm_hiY0XKz9Ge7BR8A4KXBFdpUy_9KA7t0yDheKWcdJOM-ArBjs40uUUIjyPd0tntz3tWArfLmttWkuUNI2LGmTn_EjZjBH_XRopuZNVsesynDa7C5z8a-5V_Z87OKlzkoQL8u5Tw34EYMA_0eBsdjuEs5-rU2gz80eDSdIf9e0Pf7vWxy8tPxRjNe6DROsj5-m-CEys1SIoF_Uaw0jCZo8lZRRFk_ezLpnp2zclT0dk3iCxJDPeQAAAAHjZt-vAA")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -90,18 +90,18 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://files.catbox.moe/rca1m3.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/9cevdg.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/12c791.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/i493lf.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/i0qmgf.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/8e3552aa743ffdb6f18c9.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/8e3552aa743ffdb6f18c9.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/12c791.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/p6iyy9.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/wrsicv.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/m1cye7.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/9sx30u.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/ack2ez.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/wyk2qj.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/ambjn5.MP4"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/1ligtm.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/1ligtm.jpg"
 
 
 def time_to_seconds(time):
