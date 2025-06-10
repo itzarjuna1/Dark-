e1a53f7cff0c8bc1526e45bff889d7c99bd7c508
itzarjuna1 <pachangedinanath@gmail.com>
2025-06-10 15:13:40 +0530

    Create __main__.py

---
+++ DARK/__main__.py
@@ -0,0 +1,52 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+
+import config
+from DARK import LOGGER, app, userbot
+from DARK.core.call import KUZE
+from DARK.misc import sudo
+from DARK.plugins import ALL_MODULES
+from DARK.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error(
+            "𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 V2 𝐒𝐞𝐬𝐬𝐢𝐨𝐧🤬"
+        )
+
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("DARK.plugins" + all_module)
+    LOGGER("DARK.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+    await userbot.start()
+    await KUZE.start()
+    await KUZE.decorators()
+    LOGGER("DARK").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 KUZE♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("DARK").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 KUZE♨️\n╚═════ஜ۩۞۩ஜ════╝")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
