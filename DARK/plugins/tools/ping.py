78b1923e8ba8f92f6c23b016dbb34e934341fb5b
itzarjuna1 <pachangedinanath@gmail.com>
2025-06-12 14:43:05 +0530

    Update ping.py

--- DARK/plugins/tools/ping.py
+++ DARK/plugins/tools/ping.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup
 from DARK import app
-from DARK.core.call import RAUSHAN
+from DARK.core.call import KUZE
 from DARK.utils import bot_sys_stats
 from DARK.utils.inline.extras import botplaylist_markup
 from DARK.utils.decorators.language import language
@@ -19,7 +19,7 @@ async def ping_com(client, message: Message, _):
         caption=_["ping_1"].format(app.mention),
     )
     start = datetime.now()
-    pytgping = await RAUSHAN.ping()
+    pytgping = await KUZE.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit(
