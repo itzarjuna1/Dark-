2b9a002f4446126d9c9cf1c1de469b74e6ea55da
itzarjuna1 <pachangedinanath@gmail.com>
2025-06-12 14:47:05 +0530

    Update queue.py

--- DARK/plugins/tools/queue.py
+++ DARK/plugins/tools/queue.py
@@ -8,7 +8,7 @@ from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 import config
 from DARK import app
 from DARK.misc import db
-from DARK.utils import RAUSHANBin, get_channeplayCB, seconds_to_min
+from DARK.utils import KUZEBin, get_channeplayCB, seconds_to_min
 from DARK.utils.database import get_cmode, is_active_chat, is_music_playing
 from DARK.utils.decorators.language import language, languageCB
 from DARK.utils.inline import queue_back_markup, queue_markup
@@ -176,7 +176,7 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
             return await CallbackQuery.edit_message_text(msg, reply_markup=buttons)
         if "✨" in msg:
             msg = msg.replace("✨", "")
-        link = await RAUSHANBin(msg)
+        link = await KUZEBin(msg)
         med = InputMediaPhoto(media=link, caption=_["queue_3"].format(link))
         await CallbackQuery.edit_message_media(media=med, reply_markup=buttons)
     else:
