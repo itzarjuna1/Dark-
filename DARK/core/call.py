72613060d5f2ba172086d436c02921ba41ccd216
itzarjuna1 <pachangedinanath@gmail.com>
2025-06-12 17:45:47 +0530

    Update call.py

--- DARK/core/call.py
+++ DARK/core/call.py
@@ -6,7 +6,7 @@ from typing import Union
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls
-from pytgcalls.types.input_stream import AudioPiped 
+from pytgcalls.types.stream import StreamType
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
