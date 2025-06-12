6aca1ff26306706112e40e1c8ad1de2bf6165367
itzarjuna1 <pachangedinanath@gmail.com>
2025-06-12 14:18:57 +0530

    Update vctools.py

--- DARK/plugins/tools/vctools.py
+++ DARK/plugins/tools/vctools.py
@@ -1,13 +1,13 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from SONALI import app
+from DARK import app
 from pyrogram import *
 from pyrogram.types import *
 from config import OWNER_ID
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
 from pyrogram.raw.types import InputGroupCall
-from SONALI.utils.database import get_assistant
+from DARK.utils.database import get_assistant
 from telethon.tl.functions.phone import (
     CreateGroupCallRequest,
     DiscardGroupCallRequest,
@@ -19,13 +19,13 @@ from telethon.tl.functions.phone import (
 # vc on
 @app.on_message(filters.video_chat_started)
 async def brah(_, msg):
-    await msg.reply("**😍ᴠɪᴅᴇᴏ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ🥳**")
+    await msg.reply("**🍃ᴠɪᴅᴇᴏ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ💫**")
 
 
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
-    await msg.reply("**😕ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ💔**")
+    await msg.reply("**👀ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ🫂**")
 
 
 # invite members on vc
